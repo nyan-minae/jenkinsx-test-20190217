50efe8757475d6fc72f460dc9267f8aaaed08e76
minae.nakagawa@gmail.com <minae.nakagawa@gmail.com>
2019-02-17 20:33:05 +0900

    Initial import

---
+++ src/main/java/com/example/jenkinsxtest20190217/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsxtest20190217;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
